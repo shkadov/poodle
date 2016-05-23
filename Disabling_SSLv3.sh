0ebc4fc3e7396be9a29aee87e4ba5f258d2fbd0e
shkadov <shkadov@gmail.com>
2016-05-23 23:41:33 +0300

    comments about dovecot & exim changes

--- Disabling_SSLv3.sh
+++ Disabling_SSLv3.sh
@@ -4,7 +4,7 @@
 #
 # Action Disable Support for SSLv3 on a cPanel on target VM.
 
-        
+
 case "${1}" in
         outputFormat)
                 echo "Apache|WHM|Dovecot|Courier|FTP SSL|Exim|FDisk"
@@ -13,13 +13,13 @@ case "${1}" in
 #Apache
         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'cat /usr/local/apache/conf/includes/pre_main_global.conf &> /dev/null || echo err')
 	if [ "x${status}" == "xerr" ]
-	then 
+	then
 		ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'mkdir -p /usr/local/apache/conf/includes && touch /usr/local/apache/conf/includes/pre_main_global.conf && echo -en "SSLProtocol -All +TLSv1 \nSSLHonorCipherOrder On\n"> /usr/local/apache/conf/includes/pre_main_global.conf'
 		output=$(echo "Created")
 	else
 		entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} 'grep -iE "SSLProtocol.*|SSLHonorCipherOrder.*" /usr/local/apache/conf/includes/pre_main_global.conf')
 		if [ "x${entry}" == "x" ]
-			then 
+			then
 			ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} 'sed -i 's/^.*SSLProtocol.*/SSLProtocol All +TLSv1/;s/^.*SSLHonorCipherOrder.*/SSLHonorCipherOrder On/' /usr/local/apache/conf/includes/pre_main_global.conf'
 			output=$(echo "Changed")
 		fi
@@ -32,35 +32,35 @@ case "${1}" in
         output=$(echo "Updated")
 '
 #Dovecot & Courier
-        
+
         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} '/usr/local/cpanel/scripts/setupmailserver --current |grep -i "dovecot" &> /dev/null')
         if [ "x${status}" == "xCurrent mailserver type: dovecot" ]
-        then 
+        then
                 mainfile=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'cat /var/cpanel/templates/dovecot2.2/main.local &> /dev/null || echo err')
                 if [ "x${mainfile}" == "xerr" ]
-                then 
+                then
                 ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'cp /var/cpanel/templates/dovecot2.2/main.default /var/cpanel/templates/dovecot2.2/main.local && sed -i 's/SSLv2/!SSLv2/g' /var/cpanel/templates/dovecot2.2/main.local && sed -i 's/SSLv3/!SSLv3/g' /var/cpanel/templates/dovecot2.2/main.local'
                 output=$(echo "Created")
                 fi
         else
                 entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} 'grep -iE "SSLv2|SSLv3" /var/cpanel/templates/dovecot2.2/main.local')
                 ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'sed -i 's/SSLv2/!SSLv2/g';sed -i 's/SSLv3/!SSLv3/g' /var/cpanel/templates/dovecot2.2/main.local'
-                output=$(echo "Changed")        
+                output=$(echo "Changed")
         fi
                 ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} '/usr/local/cpanel/scripts/builddovecotconf && /scripts/restartsrv_dovecot'
         if [ "x${status}" == "xCurrent mailserver type: courier" ]
-        then 
+        then
                 mainfile=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'cat /var/cpanel/courierconfig.yaml &> /dev/null')
-                
+
                 ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'sed -i 's/"TLS_PROTOCOL":.*/"TLS_PROTOCOL": 'TLSv1'/';sed -i 's/"TLS_STARTTLS_PROTOCOL":.*/"TLS_STARTTLS_PROTOCOL": 'TLSv1'/' /var/cpanel/courierconfig.yaml'
-                output=$(echo "Changed")        
+                output=$(echo "Changed")
         fi
-                ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} '/usr/local/cpanel/bin/build_courier_conf'        
+                ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} '/usr/local/cpanel/bin/build_courier_conf'
 
 #FTP
         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'grep ftpserver /var/cpanel/cpanel.config &> /dev/null || echo err')
         if [ "x${status}" == "ftpserver=pure-ftpd" ]
-        then 
+        then
                 ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'sed -i 's/^.*TLSCipherSuite:.*/TLSCipherSuite: HIGH:MEDIUM:+TLSv1:!SSLv2:+SSLv3/' /var/cpanel/conf/pureftpd/main'
                 ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} '/usr/local/cpanel/bin/build_ftp_conf && service pure-ftpd restart'
         else
@@ -70,16 +70,16 @@ case "${1}" in
                 output=$(echo "Changed")
 #Exim
 
-         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'cat /etc/exim.conf.local &> /dev/null || echo err')               
+         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'cat /etc/exim.conf.local &> /dev/null || echo err')
          if [ "x${status}" == "xerr" ]
-        then 
+        then
                 ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} 'touch /etc/exim.conf.local && echo -en "@CONFIG@\n
 tls_require_ciphers = ALL:-SSLv3:!ADH:RC4+RSA:+HIGH:+MEDIUM:-LOW:-SSLv2:-EXP\n"> /etc/exim.conf.local'
                 output=$(echo "Created")
         else
                 entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} 'grep -i "tls_require_ciphers = " /etc/exim.conf.local')
                 if [ "x${entry}" == "x" ]
-                        then 
+                        then
                         ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} 'sed -i 's/^.*tls_require_ciphers =.*/tls_require_ciphers = ALL:-SSLv3:!ADH:RC4+RSA:+HIGH:+MEDIUM:-LOW:-SSLv2:-EXP/' /etc/exim.conf.local'
                         output=$(echo "Changed")
                 fi
@@ -94,5 +94,5 @@ echo "${outPut}"
 exit 0
 
 
-
-
+#Dovecot в файле /etc/dovecot/dovecot.conf добавляем ssl_protocols = !SSLv2 !SSLv3 перед строкой ssl_cipher_list блаблабла, рестартуем довкот
+#Exim в файле /etc/exim.conf меняем tls_require_ciphers = блаблабла на tls_require_ciphers = ALL:!ADH:+HIGH:+MEDIUM:-LOW:-SSLv2:-SSLv3:-EXP. Рестартуем exim
